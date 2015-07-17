5a056f9520874e81513d3c5c46d9c11327c9811a
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2015-07-17 16:35:57 +0200

    Create openPorts.sh

---
+++ openPorts.sh
@@ -0,0 +1,7 @@
+echoping -4 -v localhost
+
+# Installing the [xinetd] packet, then enabling in the file [ /etc/xinetd.d/echo ]
+
+# If the port [7] is close, look with the [ nmap ] command (as a root): 
+
+nmap -sV -Pn -p 7 localhost
