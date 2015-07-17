dd1954b37d804bccedd406569bd7f3d59338cd0c
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2015-07-17 15:55:33 +0200

    Create iwconfig.sh

---
+++ iwconfig.sh
@@ -0,0 +1,10 @@
+## Reply of the execution of iwconfig command.
+
+wlan1     IEEE 802.11bgn  ESSID:"2400"  
+          Mode:Managed  Frequency:2.447 GHz  Access Point: 64:70:02:B5:DA:FF   
+          Bit Rate=18 Mb/s   Tx-Power=20 dBm   
+          Retry  long limit:7   RTS thr=2347 B   Fragment thr:off
+          Power Management:off
+          Link Quality=70/70  Signal level=-30 dBm  
+          Rx invalid nwid:0  Rx invalid crypt:0  Rx invalid frag:0
+          Tx excessive retries:0  Invalid misc:895   Missed beacon:0
