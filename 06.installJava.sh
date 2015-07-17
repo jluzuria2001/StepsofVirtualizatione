95fbb169d738562ca08af3b8f39d912476451e8f
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2015-07-17 11:24:21 +0200

    Create 06.installJava.sh

---
+++ 06.installJava.sh
@@ -0,0 +1,7 @@
+# Installing jdk in ubuntu 12.04 (64 bit)
+# _______________________________________
+
+sudo apt-get install python-software-properties 
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
